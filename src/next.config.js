1a14049e40345369c339547861ec9226a05de90c
Louis Law, louiscklaw <louiscklaw@users.noreply.github.com>
2024-06-09 22:33:40 +0800

    update nextjs-upload-file-tryout,

--- src/next.config.js
+++ src/next.config.js
@@ -1,7 +1,7 @@
 // const { i18n } = require('./next-i18next.config')
 
 module.exports = {
-  // output: 'export',
+  output: 'export',
   // i18n,
   trailingSlash: true,
   experimental: { instrumentationHook: true },
