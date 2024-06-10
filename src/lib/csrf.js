aae2d12582092fa8fa6b2f2c982b890685e56573
Louis Law, louiscklaw <louiscklaw@users.noreply.github.com>
2024-06-10 20:00:44 +0800

    update fix csrf,

--- src/lib/csrf.js
+++ src/lib/csrf.js
@@ -7,6 +7,8 @@ let csrf_token = '1234567890';
 const { csrf, setup } = nextCsrf({
   // eslint-disable-next-line no-undef
   secret: csrf_token,
+  httpOnly: true,
+  ignoredMethods: ['OPTIONS'],
 });
 
 export { csrf, setup };
